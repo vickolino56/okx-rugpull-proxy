5c8cdfc3d86fd0ff7b7ad0a304472bfc21d72823
vickolino56 <desouza.viniciusf@gmail.com>
2026-04-15 00:19:14 -0300

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,8 +1,8 @@
 const https = require("https")
 module.exports = function handler (req, res) {
-  res.setHeader("Access-control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-control-allow-headers", "Content-type");
+  res.setHeader("Access-Control-Allow-Headers", "Content-type");
 
   if (req.method === "options") {
     res.status(200).end();
